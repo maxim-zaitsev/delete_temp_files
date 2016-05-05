cff3b1c1534fa0d047566c8a1ed600b3861b656b
zaitsevmaxim80 <zaitsevmaxim@yandex.ru>
2016-05-05 23:31:51 +0300

    Update

--- clear_temp.bat
+++ clear_temp.bat
@@ -8,25 +8,27 @@ for /F "tokens=2 delims== skip=2" %%a in ('WMIC LogicalDisk WHERE Caption^="C:" 
 for /F %%A in ('powershell %diskspace% / 1073741824') do set diskspace=%%A
 for /F "tokens=1 delims=," %%a in ( "%diskspace%" ) do set diskspace_gb=%%a
 echo %DATE% %start_time% START Free Space: %freediskspace_gb% GB of %diskspace_gb% GB>>logs.txt
-@echo on
 
-for /D %%f in (%WINDIR%\Temp\tmp*) do DEL /f /s /q %%f
-rem for /D %f in (%WINDIR%\Temp\tmp*) do DEL /f /s /q %f
-rem for /d %a in ("%WINDIR%\Temp\tmp*") do RMDIR /s /q "%a"
-for /d %%a in ("%WINDIR%\Temp\tmp*") do RMDIR /s /q "%%a"
-rem DEL /F /S /Q %SYSTEMDRIVE%\Temp\.
-if %Temp%==%Tmp% (
-	rem DEL /f /s /q %Temp%\tmp*
-    for /D %%f in (%Temp%\tmp*) do DEL /f /s /q %%f    
-	for /d %%a in ("%Temp%\tmp*") do RMDIR /s /q "%%a"
+rem temp folder name mask
+SET folder_mask=tmp*
+
+echo step1: delete files from "%SYSTEMDRIVE%\Temp"
+if not exist "%SYSTEMDRIVE%\Temp" (
+    echo "%SYSTEMDRIVE%\Temp" is not exist. Go next step.
 ) else (
-	DEL /f /s /q %Temp%\tmp*
-	RMDIR /s /q %Temp%\
-	DEL /f /s /q %Tmp%\tmp*
-	RMDIR /s /q %Tmp%\
+    echo "%SYSTEMDRIVE%\Temp" is exist. Let's delete all folders and files here!
+    call :DELETE_FUNC "%SYSTEMDRIVE%\Temp"   
 )
-
-@echo off
+echo step2: delete files from "%WINDIR%\Temp"
+call :DELETE_FUNC "%WINDIR%\Temp\%folder_mask%"
+if %Temp%==%Tmp% (
+    echo step3: delete files from "%Temp%"
+    call :DELETE_FUNC "%Temp%\%folder_mask%"    
+) else (
+    echo step3: delete files from "%Temp%" and from "%Tmp%"
+	call :DELETE_FUNC "%Temp%\%folder_mask%"
+    call :DELETE_FUNC "%Tmp%\%folder_mask%"       
+)
 SET end_time=%TIME%
 SET end_time=%end_time:~0,8%
 for /F %%x in ('wmic logicaldisk where name^='c:' get freespace /format:value') do set %%~x>nul
@@ -37,4 +39,12 @@ for /F %%A in ('powershell %diskspace% / 1073741824') do set diskspace=%%A
 for /F "tokens=1 delims=," %%a in ( "%diskspace%" ) do set diskspace_gb=%%a
 echo %DATE% %end_time% END   Free Space: %freediskspace_gb% GB of %diskspace_gb% GB>>logs.txt
 
-echo on
+rem exit
+
+REM deleting all files from folder then removing empty folders
+:DELETE_FUNC
+for /d %%f in (%1) do del /f /s /q "%%f"
+for /d %%a in (%1) do rmdir /s /q "%%a"
+exit /b
+
+
